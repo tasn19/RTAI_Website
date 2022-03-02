b518c2ec1965540d1066425b35afc693d83c68ed
Romina Abadi <rmn@yorku.ca>
2022-03-02 18:45:59 -0500

    dockerfile added

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def upload_file():
 #    return render_template('index.html')
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0')
-    app.run(host = "localhost", port = 8000, debug = True)
+    app.run(host='0.0.0.0', port=80)
+    #app.run(host = "localhost", port = 8000, debug = True)
