1488b0bb3af618a5efcd89aba98bdf85f02a2506
Romina Abadi <rmn@yorku.ca>
2022-03-02 20:45:50 -0500

    frontend annotation bug fixes

--- static/js/scratch.js
+++ static/js/scratch.js
@@ -2,134 +2,49 @@
 // var canvas = document.querySelector('.slider-img');
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
-
 var images = ['10.png', '100.png', '101.png', '102.png', '103.png']; //TO DO change
 var i = 0;
+var image;
+
+var all_polygons = [];
+var coordinates = [];
+var isDone=true;
 
 setImg(i);
 
-function prev(){
-	if(i <= 0) i = images.length;
-	i--;
-	return setImg(i);
+// draw image in the current canvas
+function drawImg(){
+    const imageWidth = image.width;
+    const imageHeight = image.height;
+
+    canvas.width = imageWidth* 0.2;//TODO why we are doing this?
+    canvas.height = imageHeight* 0.2;//TODO why we are doing this?
+
+    context.save();
+    context.drawImage(image, 0, 0, imageWidth, imageHeight, 0, 0, imageWidth * 0.2, imageHeight * 0.2);
+    context.restore();
 }
 
-function next(){
-	if(i >= images.length-1) i = -1;
-	i++;
-	return setImg(i);
-}
-
+// resize the canvas and draw image
 function setImg(i){
-	var image = new Image();
+    all_polygons = []; //TODO read from server
+	image = new Image();
     image.src ="static/images/"+images[i];
+
 	image.onload = function() {
-        // context.drawImage(image, 0, 0);
-
-        const imageWidth = image.width;
-        const imageHeight = image.height;
-
-        // rescaling the canvas element
-        canvas.width = imageWidth* 0.2;
-        canvas.height = imageHeight*0.2;
-        //Resizing
-        // canvas.height = window.innerHeight;
-        // canvas.width = window.innerWidth;
-        context.save();
-        context.drawImage(image, 0, 0, imageWidth, imageHeight, 0, 0, imageWidth * 0.2, imageHeight * 0.2);
-        context.restore();
-        //return canvas.setAttribute('src', "images/"+images[i]);
-
-        // enabling the brush after after the image
-        // has been uploaded
-
-        const clearElement = document.getElementById("reset");
-        clearElement.onclick = () => {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            //redraw
-            context.save();
-            context.drawImage(image, 0, 0, imageWidth, imageHeight, 0, 0, imageWidth * 0.2, imageHeight * 0.2);
-            context.restore();
-        };
-
-        console.log("all polygons defined");
-        var all_polygons = [];
-        var coordinates = [];
-        var isDone=true;
-
-        $('#save').click(function(){
-            console.log("save pushed");
-            console.log(all_polygons);
-            dataSave(images[i], all_polygons);
-        });
-
-        $('#done').click(function(){
-            isDone=true;
-            console.log("done clicked");
-            console.log(coordinates);
-            all_polygons.push(coordinates);
-            coordinates=[];
-            // dataSave(images[i], coordinates);
-        });
-
-        // const boundingPoly = document.getElementById("drawpoly");
-        // boundingPoly.onclick = () =>
-        $('#drawpoly').click(function(){
-            console.log("drawpoly");
-            isDone=false;
-
-            // ref: https://stackoverflow.com/questions/29441389/how-to-draw-polygon-on-canvas-with-mouse-clicks-pure-js
-            var cw=canvas.width;
-            var ch=canvas.height;
-            function reOffset(){
-                var BB=canvas.getBoundingClientRect();
-                offsetX=BB.left;
-                offsetY=BB.top;
-            }
-            var offsetX,offsetY;
-            reOffset();
-            window.onscroll=function(e){ reOffset(); }
-
-            context.lineWidth=2;
-            context.strokeStyle='blue';
-
-            $("#canvas").mousedown(function(e){handleMouseDown(e);});
-
-            function handleMouseDown(e){
-                  if(isDone || coordinates.length>10){
-                      context.closePath();
-                      return;
-                  } //max 10 points
-
-                  // tell the browser we're handling this event
-                  e.preventDefault();
-                  e.stopPropagation();
-
-                  mouseX=parseInt(e.clientX-offsetX);
-                  mouseY=parseInt(e.clientY-offsetY);
-                  coordinates.push({x:mouseX,y:mouseY});
-
-                  drawPolygon();
-            }
-
-            function drawPolygon(){
-                  //context.clearRect(0,0,cw,ch); //to remove extra lines between points
-                  context.beginPath();
-                  context.moveTo(coordinates[0].x, coordinates[0].y);
-                  for(index=1; index<coordinates.length;index++) {
-                    context.lineTo(coordinates[index].x, coordinates[index].y);
-                  }
-                  //context.closePath(); closing it here will create closed polygons, uncomment clearRect to remove extra lines
-                  context.stroke();
-
-            }
-            context.closePath();
-        });
-
-        // const saveButton = document.getElementById('save');  // not using
-        // saveButton.addEventListener('click', () => save(canvas));
-
+        drawImg();
     }
+}
+
+function drawLine(){
+    l = coordinates.length;
+    if (l === 1){
+        context.beginPath();
+        context.moveTo(coordinates[0].x, coordinates[0].y);
+    } else {
+        context.lineTo(coordinates[l-1].x, coordinates[l-1].y);
+    }
+    context.stroke();
 }
 
 function dataSave(imgName, outputData){
@@ -150,6 +65,85 @@ function dataSave(imgName, outputData){
         alert("Saved!");
     });
 }
+
+$("#prev").click(function (){
+	if(i <= 0) i = images.length;
+	i--;
+	return setImg(i);
+});
+
+$("#next").click(function (){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg(i);
+});
+
+$("#reset").click(function (){
+    all_polygons = [];
+    coordinates = [];
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawImg();
+});
+
+$('#save').click(function(){
+    if (isDone===false){
+        alert("please finish drawing the current polygon and then save");
+        console.log("didn't save");
+        return;
+    }
+    console.log("save pushed, all_polygons is:");
+    console.log(all_polygons);
+    dataSave(images[i], all_polygons);
+});
+
+$('#done').click(function(){
+    console.log("done clicked, coordinates:");
+    console.log(coordinates);
+    if (isDone === false){
+        console.log("done clicked adding polygon to data")
+        isDone = true;
+        context.closePath();
+        all_polygons.push(coordinates);
+        coordinates=[];
+    }
+    // dataSave(images[i], coordinates);
+});
+
+$('#drawpoly').click(function(){
+    console.log("annotate clicked");
+    if (isDone === true){
+        isDone=false;
+        coordinates = [];
+    }
+});
+
+$("#canvas").mousedown(function(e){
+
+    if(isDone /*|| coordinates.length>10*/){
+      return;
+    } //max 10 points
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    function reOffset(){
+    var BB=canvas.getBoundingClientRect();
+    offsetX=BB.left;
+    offsetY=BB.top;
+    }
+    var offsetX,offsetY;
+    reOffset();
+    window.onscroll=function(e){ reOffset(); }
+
+    context.lineWidth=2;
+    context.strokeStyle='blue';
+
+    mouseX=parseInt(e.clientX-offsetX);
+    mouseY=parseInt(e.clientY-offsetY);
+    coordinates.push({x:mouseX,y:mouseY});
+
+    drawLine();
+});
 
 // function save(canvas) {  // not using
 //   const data = canvas.toDataURL('image/png');
